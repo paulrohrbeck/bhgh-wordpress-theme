b855d87289f93e5bdb8a73f19bae8b3e533e56f1
Paul Rohrbeck <paul@paul-rohrbeck.de>
2015-06-13 14:19:07 -0700

    Revert "take out sidebar and make pages full width"
    This reverts commit 65555a8561e0e8327d464a21f56710a5d752f565.

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area col-md-12">
+	<div id="primary" class="content-area col-md-9">
 		<main id="main" class="post-wrap" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -31,4 +31,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
